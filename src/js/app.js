d5afb839ac5c8270aaf16069917d44eef4f8317a
Giovanni Aguirre <giovanni.fi05@gmail.com>
2016-04-22 21:26:30 -0500

    :tada: Bilerplate config ready

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+var TREEMApp = require('./components/TREEMApp.react')
+
+ReactDOM.render(
+  <TREEMApp />,
+  document.getElementById('treem-app')
+)
---
+++ src/js/components/TREEMApp.react.js
@@ -0,0 +1,14 @@
+
+var React = require('react')
+
+var TREEMApp = React.createClass({
+
+  render() {
+
+    return (
+      <h1>TREEM App</h1>
+    )
+  }
+})
+
+module.exports = TREEMApp
